c93027c13184903f467afba13397a8f7336353a2
Dominic van Almsick <dominicvana@gmail.com>
2023-05-02 16:55:50 +0100

    fix: carousel nav button loading state

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ async function render() {
       const component = Rocket(rocket);
       rocketsSectionElement.append(component);
     }
+
+    // bit hacky
+    const carouselNavigationLists = document.querySelectorAll(
+      '.carousel__navigation-list'
+    );
+    for (const list of carouselNavigationLists) {
+      const links = list.querySelectorAll('a');
+      links[0].classList.add('active');
+    }
   } catch (e) {
     console.error(`❌ RENDER ERROR\n${e}`);
   }
